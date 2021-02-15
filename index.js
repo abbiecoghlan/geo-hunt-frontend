5e2409a363d20adee4e76c6f4171f7285504d506
Marc <marc.ferraro1@gmail.com>
2021-02-15 17:39:09 -0600

    Basic Map Puzzle Sucess

--- index.js
+++ index.js
@@ -1,15 +1,74 @@
-// Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
+  const myLatlng = { lat: -25.363, lng: 131.044 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatlng,
+  });
+  // Create the initial InfoWindow.
+  let infoWindow = new google.maps.InfoWindow({
+    content: "Click the map to get Lat/Lng!",
+    position: myLatlng,
+  });
+  infoWindow.open(map);
+  // Configure the click listener.
+  map.addListener("click", (mapsMouseEvent) => {
+    // Close the current InfoWindow.
+    infoWindow.close();
+    // Create a new InfoWindow.
+    infoWindow = new google.maps.InfoWindow({
+      position: mapsMouseEvent.latLng,
     });
-    // The marker, positioned at Uluru
-    // const marker = new google.maps.Marker({
-    //   position: uluru,
-    //   map: map,
-    // });
+    infoWindow.setContent(
+      JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+    );
+    infoWindow.open(map);
+    const lat = mapsMouseEvent.latLng.toJSON().lat
+    const lang = mapsMouseEvent.latLng.toJSON().lng
+
+    if (verify(-33.85291132953081, 151.20974884396315, lat, lang)){
+      console.log("YOU WIN")
+    } else {
+      console.log("try again")
+    }
+    // debugger
+  });
+}
+
+const verify = (latitude, longitude, guessLat, guessLong) => {
+  const minLat = latitude - .01
+  const minLongitude = longitude - .01
+
+  const maxLat = latitude + .01
+  const maxLong = longitude + .01
+  // debugger
+  if (guessLat >= minLat && guessLat <= maxLat && guessLong >= minLongitude && guessLong <= maxLong){
+    return true
   }
+}
+
+
+
+
+
+// function initMap() {
+//   const myLatlng = { lat: -25.363, lng: 131.044 };
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: myLatlng,
+//   });
+//   const marker = new google.maps.Marker({
+//     position: myLatlng,
+//     map,
+//     title: "Click to zoom",
+//   });
+//   // map.addListener("center_changed", () => {
+//   //   // 3 seconds after the center of the map has changed, pan back to the
+//   //   // marker.
+//   //   window.setTimeout(() => {
+//   //     map.panTo(marker.getPosition());
+//   //   }, 3000);
+//   // });
+//   marker.addListener("click", () => {
+//     console.log('button pressed!')
+//   });
+// }
