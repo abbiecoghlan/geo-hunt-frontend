b4be6f759b0d3508ec8b567a8ae27298c33937e6
Marc <marc.ferraro1@gmail.com>
2021-02-17 19:19:48 -0600

    Leaderboard Js beginnings

--- leaderboard.js
+++ leaderboard.js
@@ -1,6 +1,16 @@
 const fetchStatistics = () => {
-    fetchData(attemptsUrl)
-    .then(attempts => console.log(attempts))
+    fetchData('http://localhost:3000/users')
+    .then(users => renderLeaderboards(users))
+}
 
-    
+const renderLeaderboards = (users) => {
+    // users.forEach(attempt => console.log)
+    console.table(users)
+
+    mostPuzzlesCompleted(users)
+}
+
+const mostPuzzlesCompleted = (users) => {
+    // Math
+    debugger
 }
