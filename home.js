c2ce80ef88b58780d8cc92fc78286f8755cb4fc2
Marc <marc.ferraro1@gmail.com>
2021-02-19 10:33:56 -0600

    Home Page edits

--- home.js
+++ home.js
@@ -8,7 +8,17 @@ const displayHome = () => {
     homeDiv.className = 'text-center'
 
     const greeting = document.createElement('h3')
-    greeting.innerText = "welcome to ..."
-    homeDiv.append(greeting)
+    greeting.innerText = "Welcome to Geo Hunt!"
+
+    const p = document.createElement('p')
+    p.innerText = "Geo Hunt will test and expand your geographical knowledge. To get started, hit the login tab above to sign in or create an account, or check out the leaderboards and puzzle categories."
+    p.className = 'intro'
+    
+    const img = document.createElement('img')
+    img.className = 'compass'
+    
+    img.src="http://clipart-library.com/images/di45B8j6T.png"
+    
+    homeDiv.append(greeting, p, img)
     interfaceDiv.append(homeDiv)
 }
